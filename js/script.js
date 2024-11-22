3ca5027d5e8cd77249220f0314cb356b564d7598
kris.kensy <kriskensy@proton.me>
2024-11-22 16:19:27 +0100

    Add active class activation by clicked links

--- js/script.js
+++ js/script.js
@@ -6,31 +6,32 @@
     //     console.log('links:', links);
     // })
 
-    const titleClickHandler = function () {
+    const titleClickHandler = function (event) {
+        const clickedElement = this;
         console.log('Link was clicked');
-        console.log(event);
+
+        /* [DONE] remove class 'active' from all article links*/
+        const activeLinks = document.querySelectorAll('.titles a.active');
+
+        for (let activeLink of activeLinks) {
+            activeLink.classList.remove('active');
+        }
+
+        /* [DONE] add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
+        console.log('clickedElement: ', clickedElement);
+
+        /* [DONE] remove class 'active' from all articles */
+        const activeArticles = document.querySelectorAll('.posts article.active');
+
+        for (let activeArticle of activeArticles) {
+            activeArticle.classList.remove('active');
+        }
+
+        /* get 'href' attribute from the clicked link */
+        /* find the correct article using the selector (value of 'href' attribute) */
+        /* add class 'active' to the correct article */
     }
-
-    /* remove class 'active' from all article links*/
-    const activeLinks = document.querySelectorAll('.titles a.active');
-
-    for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
-    }
-
-    /* add class 'active' to the clicked link */
-
-
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts article.active');
-
-    for(let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
-    }
-    
-    /* get 'href' attribute from the clicked link */
-    /* find the correct article using the selector (value of 'href' attribute) */
-    /* add class 'active' to the correct article */
 
     const links = document.querySelectorAll('.titles a');
 
