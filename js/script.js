14b411d12c960483b917ff550c1c66e8578e8ad1
kris.kensy <kriskensy@proton.me>
2025-02-12 18:28:20 +0100

    Add calculateTagsParams function

--- js/script.js
+++ js/script.js
@@ -77,6 +77,21 @@
 
     generateTitleLinks();
 
+    const calculateTagsParams = function (tags){
+        const params = {
+            max: 0,
+            min: 999999,
+        }
+
+        for(let tag in tags){
+            // console.log(tag + ' is used ' + tags[tag] + ' times');
+            params.max = Math.max(tags[tag], params.max);
+            params.min = Math.min(tags[tag], params.min);
+        }
+
+        return params;
+    }
+
     const generateTags = function () {
         /* [NEW] create a new variable allTags with an empty object */
         let allTags ={};
@@ -126,6 +141,8 @@
         const tagList = document.querySelector(optTagsListSelector);
 
         /* [NEW] create variable for all links HTML code */
+        const tagsParams = calculateTagsParams(allTags);
+        console.log('tagsParams: ', tagsParams);
         let allTagsHTML = '';
 
         /* [NEW] START LOOP: for each tag in allTags: */
